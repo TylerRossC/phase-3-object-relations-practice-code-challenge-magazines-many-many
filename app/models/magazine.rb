c47cc4a74c31d9e6057bb341213374d1139f9987
tyler <tylercharawell@gmail.com>
2021-07-25 19:13:29 -0700

    Completed assignment

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+    def email_list
+        # self.readers.map{|reader| reader.email}.join(";") # RUBY
+        self.readers.pluck(:email).join(";")  # AR 
+    end
+
+    def self.most_popular
+        array = self.all.map{|magazine| magazine.subscriptions.count}
+        idx = array.index(array.max())
+        self.all[idx]
+    end
+
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,16 @@
 class Reader < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        Subscription.create(price: price, magazine_id: magazine.id, reader_id: self.id)
+    end
+
+    def total_subscription_price
+        self.subscriptions.pluck(:price).sum
+    end
+
+    def cancel_subscription(magazine)
+        self.subscriptions.where("magazine_id == ?", magazine.id).destroy_all
+    end
 end
